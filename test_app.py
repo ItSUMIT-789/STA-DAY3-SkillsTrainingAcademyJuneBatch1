cf4670137fe0d48156a3e27ebd90be487a0b6fd3
ItSUMIT-789 <sumitshidole789@gmail.com>
2026-06-03 15:14:59 +0530

    update

--- test_app.py
+++ test_app.py
@@ -21,4 +21,7 @@ def test_userDetails():
     response = client.get("/userDetails")
 
     assert response.status_code == 200
-    assert response.json["maritalStatus"] == "Married"
+    assert response.json["name"] == "sumit shidole"
+    assert response.json["age"] == 19
+    assert response.json["maritalStatus"] == "unmarried"
+    
